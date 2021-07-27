f025bb880463ab6d921787d585824c1f5d089421
Denis Magda <dmagda@gridgain.com>
2021-07-27 13:53:21 +0300

    Adding missing files

---
+++ src/main/java/com/gridgain/training/spring/CountryRepository.java
@@ -0,0 +1,14 @@
+package com.gridgain.training.spring;
+
+import com.gridgain.training.spring.model.Country;
+import java.util.List;
+import org.apache.ignite.springdata.repository.IgniteRepository;
+import org.apache.ignite.springdata.repository.config.RepositoryConfig;
+import org.springframework.stereotype.Repository;
+
+@RepositoryConfig(cacheName = "Country")
+@Repository
+public interface CountryRepository extends IgniteRepository<Country, String> {
+    public List<Country> findByPopulationGreaterThanOrderByPopulationDesc(int population);
+}
+
